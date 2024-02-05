75041607a96d4cd8208e9140516da0d9195ca28c
JulesCollenne <jules.collenne@univ-amu.fr>
2024-02-05 11:51:07 +0100

    fixed_dlutils

---
+++ dlutils/pytorch/jacobian.py
@@ -0,0 +1,27 @@
+import torch
+
+def jacobian(inputs, output):
+    """
+    :param inputs: Batch X Size (e.g. Depth X Width X Height)
+    :param output: Batch X Classes
+    :return: jacobian: Batch X Classes X Size
+    """
+    assert inputs.requires_grad
+
+    num_classes = output.size()[1]
+
+    J = torch.zeros(num_classes, *inputs.size())
+    grad_output = torch.zeros(*output.size())
+    if inputs.is_cuda:
+        grad_output = grad_output.cuda()
+        J = J.cuda()
+
+    for i in range(num_classes):
+        if inputs.grad is not None:
+            inputs.grad.zero_()
+        grad_output.zero_()
+        grad_output[:, i] = 1
+        output.backward(grad_output, retain_graph=True)
+        J[i] = inputs.grad.data
+
+    return torch.transpose(J, dim0=0, dim1=1)
